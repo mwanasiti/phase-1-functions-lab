ba0e95e18cc55dd2b56ddb8d3812e631db3b1a15
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-10 11:37:56 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution in this file!
+const hq=42
+const distanceFromHqInBlocks=(pickup)=>{
+    return pickup<42? hq-pickup: pickup - hq;
+}
+const distanceFromHqInFeet=(pickup)=>{
+    return distanceFromHqInBlocks(pickup) *264
+}
+const distanceTravelledInFeet=(start,destination)=>{
+    return destination<start
+    ? (start-destination)*264
+    : (destination-start)*264;
+
+}
+const calculatesFarePrice=(start,destination)=>{
+    let charges;
+    if(distanceTravelledInFeet(start,destination)<400){
+        return 0;
+    }
+    else if(
+        distanceTravelledInFeet(start,destination)>=400 &&
+        distanceTravelledInFeet(start,destination)<=2000)
+        {
+            charges=(distanceTravelledInFeet(start,destination)-400)*0.02;
+        }
+        else if(
+            distanceTravelledInFeet(start,destination)>2000 &&
+            distanceTravelledInFeet(start,destination)<=2500
+        )
+        {
+            charges=25;
+        }
+        else {
+            return "cannot travel that far";
+        }
+        return charges;
+};
+
+
+
+// function distanceFromHqInBlocks(distanceFromHqInFeet,distanceTravelledInFeet,calculatesFarePrice){
+//     this.distanceFromHqInFeet=distanceFromHqInFeet;
+//     this.distanceTravelledInFeet=distanceTravelledInFeet;
+//     this.calculatesFarePrice=calculatesFarePrice;
+
+//     this.getDistanceFromHqInBlocks= function(){
+//         console.log ()
+//     }
+// }
+
+
